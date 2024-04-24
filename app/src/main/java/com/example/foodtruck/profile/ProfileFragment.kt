5ee6e5006201ef68c1267e4a125eda86012c0359
Nir Miranda <nirmiranda2@gmail.com>
2024-04-24 16:14:40 +0300

    adding map

--- app/src/main/java/com/example/foodtruck/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/foodtruck/profile/ProfileFragment.kt
@@ -42,7 +42,7 @@ import de.hdodenhof.circleimageview.CircleImageView
 
 
 
-class ProfileFragment : Fragment(),
+class ProfileFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener,
     CommentAdapter.EditClickListener, CommentAdapter.DeleteClickListener {
 
     private val viewModel: ProfileViewModel by activityViewModels()
@@ -79,9 +79,9 @@ class ProfileFragment : Fragment(),
         commentRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
 
-//        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
-//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         return view
     }
@@ -110,79 +110,47 @@ class ProfileFragment : Fragment(),
         viewModel.fetchUserComments()
     }
 
-//    override fun onMapReady(map: GoogleMap) {
-//        googleMap = map
-//        googleMap.uiSettings.isZoomControlsEnabled = true
-//        googleMap.setOnMarkerClickListener(this@ProfileFragment)
-//        setupMap()
-//    }
-//    private fun setupMap() {
-//        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
-//            return
-//        }
-//
-//        googleMap.isMyLocationEnabled = true
-//        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
-//            if (location != null) {
-//                lastLocation = location
-//                val currentLatLng = LatLng(location.latitude, location.longitude)
-//                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
-//                addMarkers()
-//            } else {
-//                Toast.makeText(requireContext(), "Could not get current location", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    }
-//
-//    private fun addMarkers() {
-//        // Add a marker for Algorithmim
-//        val location1 = LatLng(32.109333, 34.855499)
-//        googleMap.addMarker(MarkerOptions().position(location1).title("Israel Farkash teacher of algo, Tel Aviv"))
-//
-//        // Add a marker for Rishon LeZion
-//        val location2 = LatLng(31.9585, 34.8101)
-//        googleMap.addMarker(MarkerOptions().position(location2).title("Moshe Cohen teacher of C/C++,jAVA,Rishon LeZion"))
-//
-//        // Add a marker for Jerusalem
-//        val location3 = LatLng(31.7683, 35.2137)
-//        googleMap.addMarker(MarkerOptions().position(location3).title("Yuval Cohen teacher of data structure, Jerusalem"))
-//
-//        // Add a marker for Modiin
-//        val location4 = LatLng(31.8904, 35.0057)
-//        googleMap.addMarker(MarkerOptions().position(location4).title("Chen Amrani teacher of Java,Modiin"))
-//
-//        // Add a marker for Ramat Gan
-//        val location5 = LatLng(32.0853, 34.8119)
-//        googleMap.addMarker(MarkerOptions().position(location5).title("Ori Farkash teacher of C++,Ramat Gan"))
-//
-//        // Add a marker for Petach Tikva
-//        val location6 = LatLng(32.0869, 34.8878)
-//        googleMap.addMarker(MarkerOptions().position(location6).title("Oren ShemTov teacher of Python,Petach Tikva"))
-//
-//        // Add a marker for Haifa
-//        val location7 = LatLng(32.8054, 34.9721)
-//        googleMap.addMarker(MarkerOptions().position(location7).title("Roy Ben Moshe teacher of Analiza, Haifa"))
-//
-//        // Add a marker for Ness Ziona
-//        val location8 = LatLng(31.9503, 34.7881)
-//        googleMap.addMarker(MarkerOptions().position(location8).title("Israel Israeli teacher of algo,Ness Ziona"))
-//
-//        // Add a marker for Holon
-//        val location9 = LatLng(32.0097, 34.7746)
-//        googleMap.addMarker(MarkerOptions().position(location9).title("Israel Farkash teacher of Computational models ,Holon"))
-//
-//        // Add a marker for Rosh HaAyin
-//        val location10 = LatLng(32.0649, 34.9395)
-//        googleMap.addMarker(MarkerOptions().position(location10).title("Israel Farkash teacher of Kotlin , Rosh HaAyin"))
-//
-//        // Add a marker for Beer Yaakov
-//        val location11 = LatLng(31.9167, 34.7833)
-//        googleMap.addMarker(MarkerOptions().position(location11).title("Israel Farkash teacher of C# ,Beer Yaakov"))
-//
-//        // Move the camera to the Rishon LeZion marker
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location2, 12f))
-//    }
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        googleMap.setOnMarkerClickListener(this@ProfileFragment)
+        setupMap()
+    }
+    private fun setupMap() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        googleMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                addMarkers()
+            } else {
+                Toast.makeText(requireContext(), "Could not get current location", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun addMarkers() {
+        // Add a marker for Algorithmim
+        val location1 = LatLng(32.109333, 34.855499)
+        googleMap.addMarker(MarkerOptions().position(location1).title("Hava's soup factory, Tel Aviv"))
+
+        // Add a marker for Rishon LeZion
+        val location2 = LatLng(31.9585, 34.8101)
+        googleMap.addMarker(MarkerOptions().position(location2).title("Eat with Hava,Rishon LeZion"))
+
+        // Add a marker for Jerusalem
+        val location3 = LatLng(31.7683, 35.2137)
+        googleMap.addMarker(MarkerOptions().position(location3).title("Hava's meat paradise, Jerusalem"))
+
+        // Move the camera to the Rishon LeZion marker
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location1, 12f))
+    }
 
     private fun observeUserComments() {
         viewModel.userComments.observe(viewLifecycleOwner) { comments ->
@@ -263,20 +231,20 @@ class ProfileFragment : Fragment(),
     }
 
 
-//    override fun onMarkerClick(marker: Marker): Boolean {
-//        val countryName = marker.title
-//        val bundle = Bundle().apply {
-//            putString("countryName", countryName)
-//        }
-//        val profileFragment = ProfileFragment().apply {
-//            arguments = bundle
-//        }
-//        parentFragmentManager.beginTransaction()
-//            .replace(R.id.fragment_container, profileFragment)
-//            .addToBackStack(null)
-//            .commit()
-//        return true
-//    }
+    override fun onMarkerClick(marker: Marker): Boolean {
+        val countryName = marker.title
+        val bundle = Bundle().apply {
+            putString("countryName", countryName)
+        }
+        val profileFragment = ProfileFragment().apply {
+            arguments = bundle
+        }
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, profileFragment)
+            .addToBackStack(null)
+            .commit()
+        return true
+    }
 }
 
 
